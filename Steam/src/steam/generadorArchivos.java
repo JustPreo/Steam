bd22107aabd025f01d84213895130fe0ab0e0a9f
JustPreo <impre0hn@gmail.com>
2025-09-26 08:45:54 -0600

    a

--- Steam/src/steam/generadorArchivos.java
+++ Steam/src/steam/generadorArchivos.java
@@ -4,6 +4,8 @@
  */
 package steam;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 /**
@@ -11,6 +13,46 @@ import java.io.RandomAccessFile;
  * @author user
  */
 public class generadorArchivos {
-    RandomAccessFile codigos = new RandomAccessFile("codes.stm","wr");
+
+    RandomAccessFile codigos;
+    File file;
+
+    /*
+    Formato:
+        juegos int
+        clientes int 
+        downloads int
     
+    
+     */
+    public generadorArchivos() {
+        try {
+            inicializarCarpeta();
+            codigos = new RandomAccessFile("steam/codes.stm", "rw");
+            initCodes();
+
+        } catch (IOException e) {
+            System.out.println("Error de archivos!");
+
+        }
+
+    }
+
+    public void inicializarCarpeta() {
+        file = new File("steam");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+
+    }
+
+    public void initCodes() throws IOException {
+        if (codigos.length() == 0) {
+            codigos.writeInt(1);//Juegos
+            codigos.writeInt(1);//Clientes
+            codigos.writeInt(1);//Downloads globales supongo
+            
+        }
+    }
+
 }
