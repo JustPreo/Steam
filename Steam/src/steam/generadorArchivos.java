ee1e9142244f7a825610c582125ad7d029c38826
JustPreo <impre0hn@gmail.com>
2025-09-26 08:27:18 -0600

    a

---
+++ Steam/src/steam/generadorArchivos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package steam;
+
+import java.io.RandomAccessFile;
+
+/**
+ *
+ * @author user
+ */
+public class generadorArchivos {
+    RandomAccessFile codigos = new RandomAccessFile("codes.stm","wr");
+    
+}
